cc491c9e0c2819a4df2fd7cde36e8e9bc750d72c
Mohanraj M <126415700+MaX-NeO@users.noreply.github.com>
2024-10-26 19:47:50 +0530

    D5 : Final
    Exports & Tailwind CSS , Props, Components, States

--- React/project2/src/main.jsx
+++ React/project2/src/main.jsx
@@ -1,5 +1,8 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+
+// import App from './App'
+import App from './App'
+import './assets/css/style.css'
 
 createRoot(document.getElementById('root')).render(
   <App />
