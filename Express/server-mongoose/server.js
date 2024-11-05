64ed2257d2bf058bc31a12df4a3dc4ba49d69d18
Mohanraj M <126415700+MaX-NeO@users.noreply.github.com>
2024-11-05 21:04:52 +0530

    D10 : Order & User routes,models Added

--- Express/server-mongoose/server.js
+++ Express/server-mongoose/server.js
@@ -4,6 +4,11 @@ const port = 3000
 app.use(express.json())
 const db = require('./config/db')
 const Products = require('./routes/ProductRoute')
+const Users = require('./routes/UserRoute')
+const Orders = require('./routes/OrderRoute')
 app.get('/', (req, res) => res.status(200).json({ message: "Welcome" }))
 app.use('/products', Products)
+app.use('/users', Users)
+app.use('/orders', Orders)
+
 app.listen(port, (() => console.log(`Listening on ${port}`)))
